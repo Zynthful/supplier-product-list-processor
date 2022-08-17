7e069b999ce974837391a75174fcc3725219f622
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-08-17 09:05:55 +0100

    Added additional comments

--- SupplierProductListProcessor.php
+++ SupplierProductListProcessor.php
@@ -2,9 +2,10 @@
 
 class Product
 {
-    // array of properties
+    // array of properties (e.g., make, model, colour, etc.)
     public $properties;
 
+    // constructor to assign properties
     function __construct($properties)
     {
         $this->properties = $properties;
@@ -21,18 +22,21 @@ function parse($fileName)
     $file = fopen($fileName, "r") or die("Unable to open file.");
 
     $products = array();
-    $properties = explode(",", fgets($file));
+    $properties = explode(",", fgets($file));    // assign header information as our properties
 
     // loop through each line of the file and assign properties to each product
     $count = 0;
-    while (!feof($file))
+    while (!feof($file))    // repeat until the end of the file
     {
+        // construct product with header information
         $products[$count] = new Product($properties);
         
+        // get corresponding property info from the current line
         $_properties = explode(",", fgets($file));
 
         for ($i = 0; $i < count($_properties); $i++)
         {
+            // assign corresponding property info to our product
             $products[$count]->properties[$i] = $_properties[$i];
         }
 
@@ -42,15 +46,16 @@ function parse($fileName)
     // close file after reading
     fclose($file);
 
+    // reset memory limit
     ini_set("memory_limit", $previousMemoryLimit);
 
     // print product information
     for ($i = 0; $i < count($products); $i++)
     {
-        echo "Product " . $i . PHP_EOL;
+        echo "Product " . $i . PHP_EOL; // print product index
         for ($j = 0; $j < count($properties); $j++)
         {
-            echo $properties[$j] . ": " . $products[$i]->properties[$j] . PHP_EOL;
+            echo $properties[$j] . ": " . $products[$i]->properties[$j] . PHP_EOL;  // print product properties
         }
     }
     
@@ -59,5 +64,4 @@ function parse($fileName)
 
 // todo: allow passing in arguments
 parse("D:/Projects/TBPS GitHub Test/examples/products_comma_separated.csv");
-
 ?>
